4de2123053a8fd58e62bab188962dcadb7c5fad7
JukeFinite <emmanueljucaban@gmail.com>
2022-01-25 22:26:32 -0600

    Added instance methods and hooks

--- models/User.js
+++ models/User.js
@@ -1,6 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
 const sequelize = require('../config');
-class User extends Model {}
+class User extends Model {
+//	Instance method that belongs to an instance or 1 user object from our database
+	hasPets() {
+		if (this.numberOfPets > 0) {
+			return true;
+		}
+		return false;
+	}
+}
 /*
 * username:
 * prevent null values from being saved for username,
@@ -16,7 +25,7 @@ User.init(
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				isAlphaNumeric: true,
+				isAlphanumeric: true,
 				// notNull: true,
 			},
 		},
@@ -29,16 +38,38 @@ User.init(
 		},
 		password: {
 			type: DataTypes.STRING,
+			allowNull: false,
 			validate: {
-				notNull: true,
 				len: [8],
 			}
 		},
+		numberOfPets: {
+			type: DataTypes.INTEGER,
+			default: 0,
+		}
 	},
 	{
 		sequelize,
 		timestamps: false,
 		modelName: 'User',
+		hooks: {
+			beforeCreate: async (user, options) => {
+				console.log(options);
+				console.log(user);
+				const salt = await bcrypt.genSalt(10);
+				const hashedPassword = await bcrypt.hash(user.password, salt);
+				user.email = user.email.toLowerCase();
+				user.password = hashedPassword;
+				return user;
+			},
+			beforeUpdate: async (user, options) => {
+				console.log(options);
+				console.log(user.password);
+				user.email = user.email.toLowerCase();
+				return user;
+			},
+		},
 	}
 );
 module.exports = User;
+
--- routes/api/userRoutes/index.js
+++ routes/api/userRoutes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../../../models/User');
 // Create user   /api/users
 router.post('/', async (req, res) => {
@@ -6,15 +7,18 @@ router.post('/', async (req, res) => {
 		username,
 		email,
 		password,
+		numberOfPets,
 	} = req.body;
 	if (!username || !email || !password) {
 		return res.status(400).json({ error: 'You must provider username, password, and email'});
 	}
+	// PaSsWoRd
 	try {
 		const newUser = await User.create({
 			username,
 			email,
-			password
+			password,
+			numberOfPets,
 		});
 		res.json(newUser);
 	} catch (e) {
@@ -57,7 +61,8 @@ router.patch('/:userId', async (req, res) => {
 			{
 				where: {
 					id: req.params.userId,
-				}
+				},
+				individualHooks: true,
 			}
 		);
 		const user = await User.findByPk(req.params.userId);
@@ -75,6 +80,53 @@ router.delete('/:userId', async (req, res) => {
 			}
 		});
 		res.json(deletedUser);
+	} catch (e) {
+		res.json(e);
+	}
+});
+// For logging in a user to our app
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(401).json({ error: 'You must provide a valid email and password'});
+	}
+	try {
+		//	find the user with the given email
+		const user = await User.findOne({
+			where: {
+				email,
+			}
+		});
+		//	check if the user actually exists with that given email
+		if (!user) {
+//	if no user exists, give them a 400
+			return res.status(400).json({ error: 'No user with that email'});
+		}
+//	take the users hashed password and compare it with the password that they're passing in from the form
+		const isMatchingPassword = await bcrypt.compare(password, user.password)
+		if (!isMatchingPassword) {
+			return res.status(401).json({ error: 'Invalid password'});
+		}
+		res.json({ message: 'You are now logged in successfully'});
+	} catch (e) {
+		res.json(e);
+	}
+});
+// router for getting a user by their id and checking if they have a pet or not
+//  localhost:3001/api/users/hasPets/:idWildcard
+router.get('/hasPets/:userId', async (req, res) => {
+	try {
+		const user = await User.findByPk(req.params.userId);
+		if (!user) {
+			return res.status(404).json({ error: 'User not found'});
+		}
+
+		const doesHavePets = user.hasPets();
+
+		if (!doesHavePets) {
+			return res.status(400).json({ message: 'User has no pets'});
+		}
+		res.json({ message: 'User does have pets'});
 	} catch (e) {
 		res.json(e);
 	}
